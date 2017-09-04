3f251f196f0fc00bb0fa91793ecbe79715857935
Alex Aheyev <ah.alesha@gmail.com>
2017-09-04 20:29:23 +0300

    lets try

--- SpringHiberStarter/src/main/java/org/itstep/App.java
+++ SpringHiberStarter/src/main/java/org/itstep/App.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
-import org.itstep.App;
 
 import lombok.extern.slf4j.Slf4j;
 
