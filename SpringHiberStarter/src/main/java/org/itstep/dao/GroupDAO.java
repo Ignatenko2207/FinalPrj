3c1a1b0d451ac67f19f452e7a3e9c55f7404fad3
KaPLuN <kaplun22@hotmail.com>
2017-09-15 20:55:05 +0300

    Created GroupDAO Query

--- SpringHiberStarter/src/main/java/org/itstep/dao/GroupDAO.java
+++ SpringHiberStarter/src/main/java/org/itstep/dao/GroupDAO.java
@@ -2,13 +2,14 @@ package org.itstep.dao;
 
 import org.itstep.dao.pojo.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface GroupDAO  extends JpaRepository<Group,String>{
 
-   public List<Group> getAllByCourse(int courese);
 
 
+    @Query(value = "SELECT * FROM GROUPS WHERE COURSE = ?1",nativeQuery = true)
     List<Group> findAllByCourse(int course);
 }
