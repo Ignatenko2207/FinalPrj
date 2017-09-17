464c014ef5cd13a53752fb0daad67245606c9f25
ignatenko2207 <ignatenko2207@Alex2207>
2017-09-17 11:22:40 +0300

    Made base for tests

--- SpringHiberStarter/src/main/java/org/itstep/dao/pojo/Student.java
+++ SpringHiberStarter/src/main/java/org/itstep/dao/pojo/Student.java
@@ -31,6 +31,5 @@ public class Student{
 	private String studentGroup;
 	
 	public Student() {
-		// TODO Auto-generated constructor stub
 	}
 }
---
+++ SpringHiberStarter/src/test/java/org/itstep/controller/GroupControllerTest.java
@@ -0,0 +1,29 @@
+package org.itstep.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupControllerTest {
+
+	@Test
+	public void testSaveGroup() {
+	}
+
+	@Test
+	public void testUpdateGroup() {
+	}
+
+	@Test
+	public void testGetGroup() {
+	}
+
+	@Test
+	public void testGetGroupsByCourse() {
+	}
+
+	@Test
+	public void testDeleteGroup() {
+	}
+
+}
