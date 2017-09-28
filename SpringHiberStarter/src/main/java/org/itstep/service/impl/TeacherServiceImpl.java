83966d8cd3f4918ef03b4342a26d43cc76b2e0c8
jGold6001 <jgold6001@gmail.com>
2017-09-28 19:54:05 +0300

    (:

--- SpringHiberStarter/src/main/java/org/itstep/service/impl/TeacherServiceImpl.java
+++ SpringHiberStarter/src/main/java/org/itstep/service/impl/TeacherServiceImpl.java
@@ -6,7 +6,9 @@ import org.itstep.dao.TeacherDAO;
 import org.itstep.dao.pojo.Teacher;
 import org.itstep.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TeacherServiceImpl implements TeacherService {
 	@Autowired
 	TeacherDAO teacherDAO;
