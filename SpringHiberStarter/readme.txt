0713f03936aee53e970c931b9b8bd71404f98974
Alex Aheyev <ah.alesha@gmail.com>
2017-10-06 20:41:05 +0300

    trying to swagger

--- SpringHiberStarter/readme.txt
+++ SpringHiberStarter/readme.txt
@@ -1,5 +1,2 @@
 DB: POSTGRESQL !!!
 v1.4 Beta
-
-27.09.2017
-Still conflicts with master
--- SpringHiberStarter/src/main/java/org/itstep/config/SwaggerConfig.java
+++ SpringHiberStarter/src/main/java/org/itstep/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.UiConfiguration;
@@ -20,20 +21,13 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         log.info("Swagger config");
-        return new Docket(DocumentationType.SWAGGER_2).groupName("Project REST Service")
-                .apiInfo(new ApiInfoBuilder().title("Itstep REST API").description("All the methods of the REST API").build()).select().apis(
-                        RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+        return new Docket(DocumentationType.SWAGGER_2)  
+                .select()                                  
+                .apis(RequestHandlerSelectors.any())              
+                .paths(PathSelectors.any())                          
+                .build();  
     }
 
-    @Bean
-    UiConfiguration uiConfig() {
-        return new UiConfiguration("validatorUrl", // url
-                "none", // docExpansion => none | list
-                "alpha", // apiSorter => alpha
-                "model", // defaultModelRendering => schema
-                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, // enableJsonEditor => true | false
-                true, // showRequestHeaders => true | false
-                60000L); // requestTimeout => in milliseconds, defaults to null (uses jquery xh timeout)
-    }
+    
+    
 }
